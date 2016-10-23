4d282ec49f6a3b4cfb6e56a10aa5b3579a110f48
Peter C <peter.chau.esq@gmail.com>
2016-10-23 08:48:24 +0000

    fixed test typo

--- src/Jake.test.js
+++ src/Jake.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 // import component 
-import Jake from './App';
+import Jake from './Jake';
 
 it('renders without crashing', () => {
   shallow(<Jake />);
