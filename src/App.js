dd944ac557026f35b9f63c7a34a2823c829b8f08
Peter C <peter.chau.esq@gmail.com>
2016-10-23 00:15:24 +0000

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// import css
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          land_of_ooo.vr
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
