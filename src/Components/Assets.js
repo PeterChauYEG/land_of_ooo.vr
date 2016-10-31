3251b4e71ad8739d96bc5b00bfe70dd4a0dfe040
Peter C <peter.chau.esq@gmail.com>
2016-10-31 02:52:49 +0000

    added and placed lady

--- src/Components/Assets.js
+++ src/Components/Assets.js
@@ -5,11 +5,17 @@ class Assets extends React.Component {
   render() {
     return (
       <a-assets>
+        {/* environment */}
         <img id="background" src="images/background.png" role="presentation"/>
-        <img id="Finn" src="images/finn.svg" role="presentation"/>
-        <img id="Jake" src="images/jake.svg" role="presentation"/>
+
+        {/* scene */}
         <img id="Logo" src="images/adventure_time_logo.svg" role="presentation"/>
         <img id="Treehouse" src="images/treehouse.png" role="presentation"/>
+
+        {/* NPCs */}
+        <img id="Finn" src="images/finn.svg" role="presentation"/>
+        <img id="Jake" src="images/jake.svg" role="presentation"/>
+        <img id="Lady" src="images/lady.png" role="presentation"/>
       </a-assets>
     );
   }
--- src/Components/VRScene.js
+++ src/Components/VRScene.js
@@ -11,14 +11,22 @@ class VRScene extends React.Component {
   render() {
     return (
       <Scene>
+        {/* setup */}
         <Assets />
         <Camera />
+
+        {/* lighting
         <Entity light="type: ambient; color: #fff; intensity: 1.25;"></Entity>
+        */}
+
+        {/* environment */}
         <Entity
           geometry={{primitive: 'sphere', radius: 100}}
           material={{shader: 'flat', src: "#background"}}
           rotation="0 90 0"
           scale="1 1 -1" />
+
+        {/* scene*/}
         <Entity
           geometry={{primitive: 'plane'}}
           material={{transparent: true, src: "#Logo"}}
@@ -27,22 +35,30 @@ class VRScene extends React.Component {
           scale="10 10 1" />
         <Entity
           geometry={{primitive: 'plane'}}
+          material={{transparent: true, src: "#Treehouse"}}
+          position="55.53 17.04 42.86"
+          rotation="0 -124.26 0"
+          scale="75 75 1" />
+
+        {/* NPCs */}
+        <Entity
+          geometry={{primitive: 'plane'}}
           material={{transparent: true, src: "#Finn"}}
           position="-1 1 -3"
           rotation="0 45 0"
           scale="1 1 1" />
+        <Entity
+          geometry={{primitive: 'plane'}}
+          material={{transparent: true, src: "#Lady"}}
+          position="5 5 -13"
+          rotation="0 -90 0"
+          scale="5 5 1" />
         <JakeTeleport
           from="1 1 -3"
           rotateFrom="0 -45 0"
           rotateTo="0 -90 0"
           src="#Jake"
           to="6 0.5 0.50" />
-        <Entity
-          geometry={{primitive: 'plane'}}
-          material={{transparent: true, src: "#Treehouse"}}
-          position="55.53 17.04 42.86"
-          rotation="0 -124.26 0"
-          scale="75 75 1" />
       </Scene>
     );
   }
